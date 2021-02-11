6c2577758cc8d90a10dbb27ea6af9fd21e7352db
rianrp <67884987+rianrp@users.noreply.github.com>
2021-02-11 18:01:01 -0300

    Create Main.lua
    Código de como mover o objeto, feito pelo Android

---
+++ Main.lua
@@ -0,0 +1,35 @@
+function love.load()
+  circle = {}
+  mouse = {}
+  circle.speed = 250
+  mouse.x, mouse.y = love.mouse.getPosition()
+  circle.x, circle.y = mouse.x, mouse.y
+ end
+ 
+function love.mousepressed(x, y, button)
+  if button == 1 then
+     mouse.x, mouse.y = x, y
+   end
+end
+
+function love.update(dt)
+  if circle.x < mouse.x then
+    circle.x = circle.x + (circle.speed * 2.5 * dt)
+    end
+    if circle.x > mouse.x then
+    circle.x = circle.x - (circle.speed * 2.5 * dt)
+    end
+    if circle.y < mouse.y then
+    circle.y = circle.y + (circle.speed * 2.5 * dt)
+    end
+    if circle.y > mouse.y then
+    circle.y = circle.y - (circle.speed * 2.5 * dt)
+    end
+
+end
+
+function love.draw()
+   love.graphics.setColor(1,0,0)
+   love.graphics.circle("fill", circle.x, circle.y, 50)
+end
+
